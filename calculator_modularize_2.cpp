2f9203e3425884d563eae1454092f03ff1189cc2
Emi <emi02cd@gmail.com>
2016-06-14 00:33:49 +0900

    prototype. This program can calculate +,-,*,/. Next version will be able to calculate ()! I try.

---
+++ calculator_modularize_2.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct token{
+	string type;
+	int number;
+};
+
+struct tokens{
+	token cont;
+	int index;
+};
+
+token readNumber(string line,int &index)
+{
+	token ret;
+	int number = 0;
+	int flag = 0;		//小数点がでてきたら旗を上げる
+	float keta = 0.1;
+	//数字ではなくなるまで読む
+	while(index < line.length() && isdigit(line[index]))
+	{
+		//読み込んだものが小数点だったら旗を立てる
+		if(line[index] == '.')
+		{
+			flag = 1;
+		}
+
+		if(!flag)
+		{
+			number = number * 10 + (line[index] - '0');
+		}else{
+			number += line[index] - '0' * keta;
+			keta *= 0.1;
+		}
+		index++;
+	}
+	ret.type = "NUMBER";
+	ret.number = number;
+	return ret;
+} 
+
+token readPlus(string line,int &index)
+{
+	token ret;
+	ret.type = "PLUS";
+	index++;
+	return ret;
+}
+
+token readMinus(string line,int &index)
+{
+	token ret;
+	ret.type = "MINUS";
+	index++;
+	return ret;
+}
+
+token multi(string line,int &index,vector<tokens> &tokens0,const char &c)
+{
+	token ret,next;
+	tokens last;
+	ret.type = "NUMBER";
+	index++;
+	last = tokens0.back();		//一つ前の要素
+	if (last.cont.type != "NUMBER")	//記号が二つ続いてたら
+	{
+		cout << "symbol error" << endl;
+	}
+	next = readNumber(line,index);
+	if (c == '*')
+	{
+		ret.number = last.cont.number * next.number;
+	}else if (c == '/')
+	{
+		ret.number = last.cont.number / next.number;
+	}
+	tokens0.pop_back();
+	return ret;
+}
+
+void tokenize(string line,vector<tokens> &tokens0)
+{
+	int index = 0;
+	tokens tokens1;
+	//tokenにわけてtokensにいれる
+	while(index < line.length()){
+		if (isdigit(line[index])){
+			tokens1.cont = readNumber(line,index);
+			tokens1.index = index;
+		}else if (line[index] == '+'){
+			tokens1.cont = readPlus(line,index);
+			tokens1.index = index;
+		}else if (line[index] == '-'){
+			tokens1.cont = readMinus(line,index);
+			tokens1.index = index;
+		}else if(line[index] == '*'){
+			tokens1.cont = multi(line,index,tokens0,'*');
+			tokens1.index = index;
+		}else if (line[index] == '/'){
+			tokens1.cont = multi(line,index,tokens0,'/');
+			tokens1.index = index;
+		}else{
+			cout << "Invalid character found: " << line[index] << endl;
+			exit(1);
+		}
+		//配列に加える！
+		tokens0.push_back(tokens1);
+	}
+}
+
+int evaluate(vector<tokens> &tokens1)
+{
+	int answer = 0;
+	//配列の最初に+をいれる！
+	tokens tokens0;
+	tokens0.cont.type = "PLUS";		//最初は必ず+!!
+	tokens1.insert(tokens1.begin(),tokens0);
+	//配列[0]には上で値を入れたからindexは1から
+	int index = 1;
+
+	while(index < tokens1.size())
+	{
+		if(tokens1[index].cont.type == "NUMBER"){
+			if (tokens1[index-1].cont.type == "PLUS"){
+				answer += tokens1[index].cont.number;
+			}else if (tokens1[index-1].cont.type == "MINUS"){
+				answer -= tokens1[index].cont.number;
+			}else{
+
+				cout << "Invalid syntax" << endl;
+			}
+		}
+		index++;
+	}
+	return answer;
+}
+
+int main(int argc, char const *argv[])
+{
+	string line = "";
+	int answer = 0;
+	std::vector<tokens> tokens0;
+
+	cout << ">";
+	cin >> line;
+	tokenize(line,tokens0);
+	answer = evaluate(tokens0);
+	cout << "ansewr = " << answer << endl;
+	return 0;
+}
